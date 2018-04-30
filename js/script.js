fd6c34020996e3876a3cea3948dc38c779be8e98
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-04-30 04:53:49 +0200

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var a = 15;
-
 var b = 7;
-
 var value = (a * a) - (2 * a * b) - (b * b);
 
 
@@ -9,6 +7,8 @@ if (value > 0) {
   console.log('Wynik dodatni');
 } else if (value < 0) {
   console.log('Wynik ujemny');
-} else if (value == 0){
+} else if (value === 0) {
   console.log('Wynik równy zero');
+} else if (isNaN(value)) {
+  console.log('Wynik nie jest numerem');
 }
